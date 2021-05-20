879739d0db099169e5c47203645e9422cc64d264
hainam01256 <42037841+hainam01256@users.noreply.github.com>
2021-05-20 17:29:34 +0700

    commit all

---
+++ SoHoaDuLieu_WebApp/BusinessObject/LoaiVanBan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessObject
+{
+    public class LoaiVanBan
+    {
+        public int? Id { get; set; }
+        public string TenLoaiVanBan { get; set; }
+        public DateTime? NgayTao { get; set; }
+        public DateTime? NgaySua { get; set; }
+        public string NguoiSua { get; set; }
+        public string NguoiTao { get; set; }
+    }
+}
---
+++ SoHoaDuLieu_WebApp/BusinessObject/VanBan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BusinessObject
+{
+    public class VanBan
+    {
+        public int Id { get; set; }
+        public string TenVanBan { get; set; }
+        public DateTime? NgayTao { get; set; }
+        public DateTime? NgaySua { get; set; }
+        public string NguoiSua { get; set; }
+        public string NguoiTao { get; set; }
+        public string NguoiDuyet1 { get; set; }
+        public string NguoiDuyet2 { get; set; }
+        public string NguoiDuyet3 { get; set; }
+        public string NoiDungBocTach { get; set; }
+        public int LoaiVanBan { get; set; }
+    }
+}
